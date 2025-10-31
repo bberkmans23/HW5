c35eb571511c296017c969889ecc5c012db218eb
bberkmans23 <bberkmans@luc.edu>
2025-10-30 20:36:52 -0500

    Update author name and section number in comments

--- BloomFilter.java
+++ BloomFilter.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Brendan Berkmans / 272 002
  *
  *   Note, additional comments provided throughout source code is
  *   for educational purposes.
